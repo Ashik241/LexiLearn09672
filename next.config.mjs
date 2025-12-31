9c272d40629902f51ff1ced114d12dc7a31298dc
Ashiqur Rahman (Ashik) <ashiqurrahman2003ashik@gmail.com>
2025-12-31 09:55:10 +0000

    unhandledRejection [Error [PageNotFoundError]: Cannot find module for pa

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Vercel deployment does not require static export.
-  // Removing `output: 'export'` will allow Vercel to use its
-  // optimized Next.js build and hosting capabilities.
+    output: 'standalone',
 };
 
 export default nextConfig;

3a7c159e8eee17e5ff88669c51433e1d70c5757d
Ashiqur Rahman (Ashik) <ashiqurrahman2003ashik@gmail.com>
2025-12-31 13:02:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-import {
-  cacheFirst,
-  staleWhileRevalidate
-} from 'workbox-recipes';
-import {
-  precacheAndRoute
-} from 'workbox-precaching';
+import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,55 +6,30 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-// PWA configuration
-const withPWA = require('next-pwa')({
+const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  buildExcludes: [/app-build-manifest.json$/],
-  cacheStartUrl: true,
-  runtimeCaching: [{
-      urlPattern: ({
-        request,
-        url
-      }) =>
-      request.destination === "document" ||
-      request.destination === "script" ||
-      request.destination === "style",
-      handler: staleWhileRevalidate({
-        cacheName: "pages-cache",
-        plugins: [{
-          // This plugin will cache responses with a 200 status code.
-          cacheableResponse: {
-            statuses: [200],
-          },
-        }, ],
-      }),
-    },
+  runtimeCaching: [
     {
-      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-      handler: cacheFirst({
-        cacheName: 'image-cache',
-        plugins: [{
-          // This plugin will cache responses with a 200 status code.
-          cacheableResponse: {
-            statuses: [200],
-          },
-        }, ],
-      }),
+      urlPattern: /^https?.*/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'all-pages-and-assets',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
     },
-    {
-      urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/,
-      handler: staleWhileRevalidate({
-        cacheName: 'google-fonts-cache',
-      }),
-    }
   ],
   fallbacks: {
     document: '/_offline',
   }
 });
 
-
-export default withPWA(nextConfig);
+export default pwaConfig(nextConfig);

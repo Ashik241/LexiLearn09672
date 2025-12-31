cf0df9d5c3a0c6a3232e5d9ae9212a0d5e67bacd
Ashiqur Rahman (Ashik) <ashiqurrahman2003ashik@gmail.com>
2025-12-31 08:19:28 +0000

    Github এ হোস্ট করার জন্য যে পরিবর্তনগুলো করেছেন এগুলো বাদ দিয়ে দেন মানে

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
-import withPWAInit from '@ducanh2912/next-pwa';
-
 /** @type {import('next').NextConfig} */
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
 const nextConfig = {
-  output: 'export',
-  basePath: '/LexiLearn09672',
-  assetPrefix: '/LexiLearn09672/',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  // Your Next.js configuration options can go here.
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;

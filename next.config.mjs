454c28b2b55cff75e02dee69d2691bf9522cdc6c
Ashiqur Rahman (Ashik) <ashiqurrahman2003ashik@gmail.com>
2025-12-31 10:32:14 +0000

    PWA function add all page function... vobullary add hobe local storage y

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,40 @@
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
 
-const nextConfig = {
-  output: 'standalone',
-};
-
-const pwaConfig = withPWA({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https\:\/\/fonts\.googleapis\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'gstatic-fonts-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        },
+      },
+    },
+  ],
 });
 
-export default pwaConfig(nextConfig);
+
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);

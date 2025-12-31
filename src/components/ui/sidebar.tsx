4e0278af28e5aee6755c07cc23da54bc22415b03
Ashiqur Rahman (Ashik) <ashiqurrahman2003ashik@gmail.com>
2025-12-31 09:41:30 +0000

    Run.. npm run build ... command and chack all error and solve..

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -38,7 +38,7 @@ type SidebarContext = {
 
 const SidebarContext = React.createContext<SidebarContext | null>(null)
 
-function useSidebar() {
+export function useSidebar() {
   const context = React.useContext(SidebarContext)
   if (!context) {
     throw new Error("useSidebar must be used within a SidebarProvider.")
@@ -759,5 +759,4 @@ export {
   SidebarRail,
   SidebarSeparator,
   SidebarTrigger,
-  useSidebar,
 }

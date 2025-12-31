56e2566278e867b7de2f844780bc035b2601804d
Ashiqur Rahman (Ashik) <ashiqurrahman2003ashik@gmail.com>
2025-12-31 07:44:54 +0000

    ei somosha abar  ken... (_for element div_)

--- src/app/vocabulary/page.tsx
+++ src/app/vocabulary/page.tsx
@@ -7,7 +7,11 @@ import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 function Loading() {
-    return <div>Loading...</div>;
+    return (
+        <div className="flex justify-center items-center p-8">
+            <div className="w-12 h-12 border-4 border-primary border-t-transparent rounded-full animate-spin"></div>
+        </div>
+    );
 }
 
 function VocabularyPageContent() {

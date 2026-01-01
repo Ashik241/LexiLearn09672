4bbd9fcfe0f7800b497b3222e05dbf1581f79eea
Ashiqur Rahman (Ashik) <ashiqurrahman2003ashik@gmail.com>
2026-01-01 13:25:18 +0000

    drawer er to color akhono thik korte paren nai...ki ken paren na..

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -198,7 +198,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className={cn("w-[--sidebar-width] p-0 text-sidebar-foreground [&>button]:hidden bg-sidebar", className)}
+            className={cn("w-[--sidebar-width] p-0 bg-sidebar text-sidebar-foreground [&>button]:hidden", className)}
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -206,6 +206,7 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+             <SheetTitle/>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
